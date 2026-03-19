f7212dc6589e62c2f9e84a6b692041da68b1b3fa
gabehanson <gabehanson@gmail.com>
2026-03-18 23:07:34 -0400

    fix: update author name to Gabriel Hanson

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ceiba",
     version="0.1.0",
-    author="Gabe Hanson",
+    author="Gabriel Hanson",
     author_email="",                        # add if desired
     description="Spatial transcriptomics methods for spatial analysis pipelines",
     long_description=open("README.md").read(),
--- src/ceiba/__init__.py
+++ src/ceiba/__init__.py
@@ -4,4 +4,4 @@ https://github.com/gabehanson/mhc2-luad-spatial
 """
  
 __version__ = "0.1.0"
-__author__ = "Gabe Hanson"
+__author__ = "Gabriel Hanson"
